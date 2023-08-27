79173a4e132f58f17ceca47164f54fbe4aade1d8
unknown <hasnainalikk@gmail.com>
2023-08-27 06:12:11 -0700

    file add

---
+++ script.js
@@ -0,0 +1,36 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBM0kHekmIMrs-gVCN9AxiPeUrq7VfFY1A",
+    authDomain: "login-page-541ae.firebaseapp.com",
+    projectId: "login-page-541ae",
+    storageBucket: "login-page-541ae.appspot.com",
+    messagingSenderId: "533689852759",
+    appId: "1:533689852759:web:728150a9b41f5992d91412"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBM0kHekmIMrs-gVCN9AxiPeUrq7VfFY1A",
+  authDomain: "login-page-541ae.firebaseapp.com",
+  projectId: "login-page-541ae",
+  storageBucket: "login-page-541ae.appspot.com",
+  messagingSenderId: "533689852759",
+  appId: "1:533689852759:web:728150a9b41f5992d91412"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
